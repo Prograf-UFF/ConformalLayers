8ec94d476186b5b44f3a4290468d09ed2a1434ad
Eduardo Vera Sousa <eduardoverasousa@gmail.com>
2021-10-28 10:28:48 -0300

    Update baseline.py

--- Experiments/networks/baseline.py
+++ Experiments/networks/baseline.py
@@ -10,11 +10,11 @@ class BaseReSProNet(nn.Module):
     def __init__(self) -> None:
         super(BaseReSProNet, self).__init__()
         self.features = cl.ConformalLayers(
-            cl.Conv2d(3, 32, kernel_size=5),
+            cl.Conv2d(3, 32, kernel_size=3),
             cl.ReSPro(),
-            cl.AvgPool2d(kernel_size=3, stride=3),
+            cl.AvgPool2d(kernel_size=2, stride=2),
         )
-        self.fc = nn.Linear(2592, 10)
+        self.fc = nn.Linear(7200, 10)
 
     def forward(self, x) -> torch.Tensor:
         x = self.features(x)
@@ -27,11 +27,11 @@ class BaseLinearNet(nn.Module):
     def __init__(self) -> None:
         super(BaseLinearNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=5),
+            nn.Conv2d(3, 32, kernel_size=3),
             # Linear activation
-            nn.AvgPool2d(kernel_size=3, stride=3),
+            nn.AvgPool2d(kernel_size=2, stride=2),
         )
-        self.fc = nn.Linear(2592, 10)
+        self.fc = nn.Linear(7200, 10)
 
     def forward(self, x):
         x = self.features(x)
@@ -44,11 +44,11 @@ class BaseReLUNet(nn.Module):
     def __init__(self) -> None:
         super(BaseReLUNet, self).__init__()
         self.features = nn.Sequential(
-            nn.Conv2d(3, 32, kernel_size=5),
+            nn.Conv2d(3, 32, kernel_size=3),
             nn.ReLU(),
-            nn.AvgPool2d(kernel_size=3, stride=3),
+            nn.AvgPool2d(kernel_size=2, stride=2),
         )
-        self.fc = nn.Linear(2592, 10)
+        self.fc = nn.Linear(7200, 10)
 
     def forward(self, x):
         x = self.features(x)
