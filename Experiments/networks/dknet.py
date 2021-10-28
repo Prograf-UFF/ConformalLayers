6235c6c1c72921773ed59f2cdedfd897bd11b2a1
Eduardo Vera Sousa <eduardoverasousa@gmail.com>
2021-10-28 10:31:14 -0300

    Update dknet.py

--- Experiments/networks/dknet.py
+++ Experiments/networks/dknet.py
@@ -80,13 +80,13 @@ class D3ModNet(nn.Module):
         super(D3ModNet, self).__init__()
         self.features = nn.Sequential(
             nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3),
-            nn.ReSPro(),
+            nn.ReLU(),
             nn.AvgPool2d(kernel_size=2, stride=2),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3),
-            nn.ReSPro(),
+            nn.ReLU(),
             nn.AvgPool2d(kernel_size=2, stride=2),
             nn.Conv2d(in_channels=32, out_channels=32, kernel_size=3),
-            nn.ReSPro(),
+            nn.ReLU(),
             nn.AvgPool2d(kernel_size=2, stride=2),
         )
         self.fc1 = nn.Linear(128, 10)
