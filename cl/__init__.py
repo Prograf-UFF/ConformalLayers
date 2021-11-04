f32528f72f5a1cf31aca98c79d5e5821c0a95127
Leandro A. F. Fernandes <laffernandes@ic.uff.br>
2021-11-04 10:37:24 -0300

    New package name, from `cl` to `ConformalLayers`.

--- cl/__init__.py
+++ cl/__init__.py
@@ -1,11 +1,3 @@
-from .activation import ReSPro
-from .convolution import Conv1d, Conv2d, Conv3d
-from .layers import ConformalLayers
-from .pooling import AvgPool1d, AvgPool2d, AvgPool3d
-from .regularization import Dropout
-from .utility import Flatten
-
-
 __all__ = [
     'AvgPool1d', 'AvgPool2d', 'AvgPool3d',
     'ConformalLayers',
@@ -14,3 +6,13 @@ __all__ = [
     'Flatten',
     'SRePro',
 ]
+__author__ = 'Eduardo V. Sousa, Leandro A. F. Fernandes, Cristina N. Vasconcelos'
+__author_email__ = 'eduardovera@ic.uff.br, laffernandes@ic.uff.br, crisnv@ic.uff.br'
+__version__ = '1.1.0'
+
+from .activation import ReSPro
+from .convolution import Conv1d, Conv2d, Conv3d
+from .layers import ConformalLayers
+from .pooling import AvgPool1d, AvgPool2d, AvgPool3d
+from .regularization import Dropout
+from .utility import Flatten
--- setup.py
+++ setup.py
@@ -1,18 +1,19 @@
+from cl import __author__, __author_email__, __version__
 import setuptools
 
 
 setuptools.setup(
-    name='cl',
-    version='1.1.0',
+    name='ConformalLayers',
+    version=__version__,
     description='The implementation of the ConformalLayers: A non-linear sequential neural network with associative layers',
-    author='Eduardo V. Sousa, Leandro A. F. Fernandes',
-    author_email='eduardovera@ic.uff.br, laffernandes@ic.uff.br',
+    author=__author__,
+    author_email=__author_email__,
     url='https://github.com/Prograf-UFF/ConformalLayers/',
     packages=setuptools.find_packages(),
     include_package_data=True,
     install_requires=[
         'torch>=1.8',
         'MinkowskiEngine>=0.5.4',
-        'tqdm',  # Just for the experiments and tests.
+        'tqdm',  # Just for the experiments.
     ],
     zip_safe=False)
